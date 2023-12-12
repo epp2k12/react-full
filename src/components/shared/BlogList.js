b00b5081334c67a2fc338eebe0083a6a3d15b110
Erwin <epp2k12@gmail.com>
2023-12-12 17:26:09 +0800

    Added: Post and Delete functions

--- src/components/shared/BlogList.js
+++ src/components/shared/BlogList.js
@@ -1,20 +1,22 @@
+import { Link } from "react-router-dom";
+
 // const BlogList = (props) => {
 const BlogList = ({ blogs, title }) => {
 
     // const blogs = props.blogs;
     // const title = props.title;
 
-
-
     return (
         <div className="blog-list">
             <h2> {title} </h2>
-            {
+            { 
                 blogs.map((data) =>
                 (
                     <div className="blog-preview" key={data.id}>
+                        <Link to={`/blogs/${data.id}`}>
                         <h2>{data.title}</h2>
                         <p>Written by: {data.author}</p>
+                        </Link>
                         {/* <button onClick={()=>{handleDelete(data.id)}}>Delete Blog</button> */}
                     </div>
                 )
